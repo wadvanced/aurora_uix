ee7c816379e7cfce060bcb4ed90dfb04404ce085
Federico Alcantara <federico.alcantara@sicflex.com>
2024-12-30 00:09:29 +0100

    Implementing route registration

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aurora-uix
-Low code UI for the elixir's Phoenix Framework
+Low code UI for the elixir's Phoenix Framework.
+
+## Contributing
+PR are welcomed, we encourage code quality, so PR must pass the mix consistency task. It do:
+* Re-formats code with mix `format`.
+* Applies strict credo analysis using mix `credo` --strict.
+* Runs dialyzer with mix `dialyzer`.
+* Verify documentation healthness with mix `doctor`.
+
+The formatter credo and doctor have configuration files tailored to this project, however changes might be accepted in corresponding PR.
+
+
---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule AuroraUix.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :aurora_uix,
+      version: "0.1.0",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.4", only: [:dev]},
+      {:doctor, "~> 0.22.0", only: :dev}
+    ]
+  end
+
+  defp aliases do
+    [
+      consistency: ["format", "credo --strict", "dialyzer", "doctor"]
+    ]
+  end
+end
