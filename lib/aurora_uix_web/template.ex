25d16e9e2ed4348d1029bda509db8bf84b196b60
Federico Alcantara <federico.alcantara@wadvanced.com>
2025-01-27 09:15:08 +0100

    Ensures that templates implements the behaviour

--- lib/aurora_uix_web/template.ex
+++ lib/aurora_uix_web/template.ex
@@ -29,4 +29,68 @@ defmodule AuroraUixWeb.Template do
 
   """
   @callback generate(type :: atom, opts :: Keyword.t()) :: Macro.t()
+
+  @doc """
+  Validates that the given module implements the current behaviour (`#{__MODULE__}`).
+
+  This function checks two things:
+  1. The module has declared the `#{__MODULE__}` behaviour via the `@behaviour` attribute.
+  2. The module exports the required functions.
+
+  If either of these conditions is not met, it raises an `ArgumentError`.
+
+  ## Parameters
+
+    - `module` (`module`): The module to validate.
+
+  ## Returns
+
+    - (`module`): The same module, if it is valid.
+
+  ## Raises
+
+    - `ArgumentError`: If the module does not implement the required behaviour or does not define the `generate/2` function.
+
+  ## Examples
+
+    ```elixir
+    defmodule ValidModule do
+      @behaviour AuroraUixWeb.Template
+      def generate(:example, _opts), do: :ok
+    end
+
+    defmodule InvalidModule do
+      def generate(:example, _opts), do: :ok
+    end
+
+    AuroraUixWeb.Template.validate(ValidModule)
+    # => ValidModule
+
+    AuroraUixWeb.Template.validate(InvalidModule)
+    # => ** (ArgumentError) The InvalidModule does not implement the `AuroraUixWeb.Template` behaviour.
+  """
+  @spec validate(module) :: module
+  def validate(module) do
+    valid? =
+      module
+      |> behaviour_implemented?()
+      |> Kernel.and(function_exported?(module, :generate, 2))
+
+    if !valid?,
+      do:
+        raise(
+          ArgumentError,
+          "The #{module} does not implement the `#{__MODULE__}` behaviour."
+        )
+
+    module
+  end
+
+  @spec behaviour_implemented?(module) :: boolean
+  defp behaviour_implemented?(module) do
+    :attributes
+    |> module.__info__()
+    |> Keyword.get(:behaviour, [])
+    |> Enum.member?(__MODULE__)
+  end
 end
