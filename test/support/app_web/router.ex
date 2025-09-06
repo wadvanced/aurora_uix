5a98bf956ae737f0bd0812666e9fd86f9e5b5f26
Federico Alcantara <federico.alcantara@wadvanced.com>
2025-09-06 22:24:47 +0200

    Manual resource definition
    Should allow the resource definition to be manually declared (no macros) with minimal effort.

--- test/support/app_web/router.ex
+++ test/support/app_web/router.ex
@@ -175,6 +175,11 @@ defmodule Aurora.Uix.Test.Web.Router do
       "infinity-scroll-"
     )
 
+    Web.register_product_crud(
+      ManualResourceTest,
+      "manual-resource-"
+    )
+
     ## You can create a file test/cases_live/-local-demo_test.exs
     ## With Aurora.Uix.Test.Web.LocalDemoTest module
     ## And then test its output in /local-demo-products
