edca829330e50e6e90a85236e0f390fec916da46
Federico Alcantara <federico.alcantara@wadvanced.com>
2025-10-06 18:12:43 +0200

    ci: add dependency-check and publish-to-hex workflows

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Aurora.Uix.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/wadvanced/aurora_uix"
-  @version "0.1.0"
+  @version "0.1.0-alpha.1"
 
   def project do
     [
