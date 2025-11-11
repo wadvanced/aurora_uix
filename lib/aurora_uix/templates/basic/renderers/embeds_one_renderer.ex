cb111a4d2ae8f073b578f19f3f02ac76b7c3c696
Federico Alcantara <federico.alcantara@wadvanced.com>
2025-11-11 16:54:34 +0100

    chore: rename embed one to embeds one to align with ecto naming

--- lib/aurora_uix/templates/basic/renderers/embeds_one_renderer.ex
+++ lib/aurora_uix/templates/basic/renderers/embeds_one_renderer.ex
@@ -1,9 +1,10 @@
-defmodule Aurora.Uix.Templates.Basic.Renderers.EmbedOneRenderer do
+defmodule Aurora.Uix.Templates.Basic.Renderers.EmbedsOneRenderer do
   @moduledoc """
   Renders an embedded one-to-one association within a form.
   """
   use Aurora.Uix.CoreComponentsImporter
 
+  alias Aurora.Uix.Helpers.Common, as: CommonHelper
   alias Aurora.Uix.Templates.Basic.Helpers, as: BasicHelpers
   alias Aurora.Uix.Templates.Basic.Renderer
 
@@ -20,22 +21,33 @@ defmodule Aurora.Uix.Templates.Basic.Renderers.EmbedOneRenderer do
   @spec render(map()) :: Phoenix.LiveView.Rendered.t()
   def render(
         %{
-          field: %{data: %{resource: embed_resource_name}},
+          field: %{data: %{resource: embed_resource_name}} = field,
           auix: %{layout_type: :form}
         } = assigns
       ) do
     layout_tree = BasicHelpers.get_layout(assigns, embed_resource_name, :form)
 
+    field =
+      assigns
+      |> BasicHelpers.get_resource(embed_resource_name, [:resource_config, :name])
+      |> CommonHelper.capitalize()
+      |> then(&struct(field, label: &1))
+
     assigns =
       assigns
       |> BasicHelpers.assign_auix(:layout_tree, layout_tree)
       |> BasicHelpers.assign_auix(:resource_name, embed_resource_name)
+      |> assign(:field, field)
 
     ~H"""
-      <.inputs_for :let={embed_form} field={@auix.form[@field.key]}>
-        <Renderer.render_inner_elements auix={Map.put(@auix, :form, embed_form)} />     
-      </.inputs_for>
-
+      <div class="auix-embeds-one-container">
+        <.header>
+          {@field.label}
+        </.header>
+        <.inputs_for :let={embed_form} field={@auix.form[@field.key]}>
+          <Renderer.render_inner_elements auix={Map.put(@auix, :form, embed_form)} />     
+        </.inputs_for>
+      </div>
     """
   end
 
