d4d4507ef036be8ae5c10075ee88b10c659dcb21
Federico Alcantara <federico.alcantara@wadvanced.com>
2025-11-23 00:13:13 +0100

    docs: remove non packageable modules from the documentation

--- lib/aurora_uix/guides/accounts.ex
+++ lib/aurora_uix/guides/accounts.ex
@@ -1,8 +1,11 @@
 defmodule Aurora.Uix.Accounts do
   @moduledoc """
-  Provides test context for user accounts in the Aurora.Uix application.
+  Provides guides and test context for user accounts in the Aurora.Uix application. 
+  This module and its children are excluded from package builds and documentation, 
+  since they are intended for use in test and dev environments only.
 
-  This module registers the test User schema with the Repo for use in test scenarios.
+
+  This module registers the User schema with the Repo for use in guides and test scenarios.
   """
 
   use Aurora.Ctx
