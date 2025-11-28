1611e1f6793b31cd2b202e81b72acd83a60b63e2
Federico Alcantara <federico.alcantara@wadvanced.com>
2025-11-28 16:04:11 +0100

    chore: comply with consistency checks

--- lib/aurora_uix_web/routes.ex
+++ lib/aurora_uix_web/routes.ex
@@ -6,7 +6,7 @@ defmodule Aurora.UixWeb.Routes do
   @doc """
   Macro to load routes for the application.
   """
-  defmacro load_routes() do
+  defmacro load_routes do
     quote do
       auix_live_resources("/guide-overview-products", Overview.Product)
     end
