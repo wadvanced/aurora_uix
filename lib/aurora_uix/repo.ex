6d3f19b90cf2a08361f3b37c472bddf41afa9b05
Federico Alcantara <federico.alcantara@wadvanced.com>
2026-01-20 05:37:55 +0100

    build: prepare ash environment and repo

--- lib/aurora_uix/repo.ex
+++ lib/aurora_uix/repo.ex
@@ -5,7 +5,15 @@ defmodule Aurora.Uix.Repo do
   ## Key Features
   - Used for database operations in test modules and helpers.
   """
-  use Ecto.Repo,
+  use AshPostgres.Repo,
     otp_app: :aurora_uix,
     adapter: Ecto.Adapters.Postgres
+
+  def installed_extensions do
+    ["uuid-ossp", "citext", "ash-functions"]
+  end
+
+  def min_pg_version do
+    %Version{major: 13, minor: 0, patch: 0}
+  end
 end
