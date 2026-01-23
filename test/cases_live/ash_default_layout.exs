dd760fabdbb68b108936a51204540f37819f7e64
Federico Alcantara <federico.alcantara@wadvanced.com>
2026-01-23 09:23:52 +0100

    test: add simple pagination presence in test

--- test/cases_live/ash_default_layout.exs
+++ test/cases_live/ash_default_layout.exs
@@ -13,10 +13,112 @@ defmodule Aurora.UixWeb.Test.AshDefaultLayoutTest do
 
   test "Default layout index", %{conn: conn} do
     delete_all_blog_data()
-    create_sample_authors(5)
+    create_sample_authors(20)
 
-    {:ok, _view, html} = live(conn, "/ash-default-layout-authors")
+    {:ok, view, html} = live(conn, "/ash-default-layout-authors")
     assert html =~ "Listing Authors"
     assert html =~ "New Author"
+
+    refute has_element?(view, "div[name='auix-pages_bar-authors']")
+  end
+
+  test "Pagination navigation test", %{conn: conn} do
+    delete_all_blog_data()
+    create_sample_authors(1000)
+    {:ok, view, html} = live(conn, "/ash-default-layout-authors")
+    assert html =~ "Listing Authors"
+
+    assert has_element?(view, "div[name='auix-pages_bar-authors']")
+
+    assert html
+           |> LazyHTML.from_document()
+           |> LazyHTML.query("#auix-table-ash-default-layout-authors-index tr")
+           |> Enum.count() == 40
+
+    assert html
+           |> LazyHTML.from_document()
+           |> LazyHTML.query(
+             "div[name='auix-pages_bar-authors-md'] [name^='auix-pages_bar_page-']"
+           )
+           |> Enum.map(&(&1 |> LazyHTML.text() |> String.trim())) == [
+             "1",
+             "2",
+             "3",
+             "4",
+             "5",
+             "6",
+             "7",
+             "8",
+             "9",
+             "...",
+             "25",
+             ""
+           ]
+
+    # Direct navigation
+    {:ok, view_direct, html_direct} = live(conn, "/ash-default-layout-authors?page=12")
+
+    assert html_direct
+           |> LazyHTML.from_document()
+           |> LazyHTML.query(
+             "div[name='auix-pages_bar-authors-md'] [name^='auix-pages_bar_page-']"
+           )
+           |> Enum.map(&(&1 |> LazyHTML.text() |> String.trim())) == [
+             "",
+             "1",
+             "...",
+             "8",
+             "9",
+             "10",
+             "11",
+             "12",
+             "13",
+             "14",
+             "15",
+             "16",
+             "...",
+             "25",
+             ""
+           ]
+
+    assert html_direct
+           |> LazyHTML.from_document()
+           |> LazyHTML.query("#auix-table-ash-default-layout-authors-index tr")
+           |> Enum.count() == 40
+
+    # Navigate to a page by click
+    view_direct
+    |> element("div[name='auix-pages_bar-authors-md'] a[name='auix-pages_bar_page-10']")
+    |> render_click()
+
+    assert view_direct
+           |> render()
+           |> LazyHTML.from_document()
+           |> LazyHTML.query(
+             "div[name='auix-pages_bar-authors-md'] [name^='auix-pages_bar_page-']"
+           )
+           |> Enum.map(&(&1 |> LazyHTML.text() |> String.trim())) == [
+             "",
+             "1",
+             "...",
+             "6",
+             "7",
+             "8",
+             "9",
+             "10",
+             "11",
+             "12",
+             "13",
+             "14",
+             "...",
+             "25",
+             ""
+           ]
+
+    assert view_direct
+           |> render()
+           |> LazyHTML.from_document()
+           |> LazyHTML.query("#auix-table-ash-default-layout-authors-index tr")
+           |> Enum.count() == 40
   end
 end
