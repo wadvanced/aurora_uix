cf9f14ebbcf89cf15292a4467aa6ec3a78ca09c3
Federico Alcantara <federico.alcantara@wadvanced.com>
2026-01-28 17:44:39 +0100

    tests: update ash test for the new cases

--- test/cases_live/ash_default_layout.exs
+++ test/cases_live/ash_default_layout.exs
@@ -156,4 +156,98 @@ defmodule Aurora.UixWeb.Test.AshDefaultLayoutTest do
               |> LazyHTML.attribute("value") == [author.bio]
     )
   end
+
+  test "Test CREATE new author", %{conn: conn} do
+    delete_all_blog_data()
+    {:ok, view, _html} = live(conn, "/ash-default-layout-authors/new")
+
+    assert view
+           |> element("div#auix-author-new-modal header")
+           |> render() =~ "New Author"
+
+    assert view
+           |> element("div#auix-author-new-modal header")
+           |> render() =~ "Creates a new <strong>Author</strong> record in your database"
+
+    # Create unique test data
+    unique_name = "Author-#{System.system_time(:nanosecond)}"
+    unique_email = "author-#{System.system_time(:nanosecond)}@test.com"
+    unique_bio = "Test bio #{System.system_time(:nanosecond)}"
+
+    # Submit the form
+    view
+    |> form("#auix-author-form",
+      author: %{
+        name: unique_name,
+        email: unique_email,
+        bio: unique_bio
+      }
+    )
+    |> render_submit()
+
+    # Verify the author appears in the index
+    {:ok, _view, new_html} = live(conn, "/ash-default-layout-authors")
+
+    assert new_html =~ "Listing Authors"
+    assert new_html =~ unique_name
+    assert new_html =~ unique_email
+  end
+
+  test "Test UPDATE existing author", %{conn: conn} do
+    delete_all_blog_data()
+
+    author =
+      1
+      |> create_sample_authors()
+      |> List.first()
+
+    {:ok, view, _html} = live(conn, "/ash-default-layout-authors/#{author.id}/edit")
+
+    assert view
+           |> element("div#auix-author-edit-modal header")
+           |> render() =~ "Edit Author"
+
+    assert view
+           |> element("div#auix-author-edit-modal header")
+           |> render() =~
+             "Use this form to manage <strong>Authors</strong> records in your database"
+
+    # Verify form fields are populated with existing values
+    html = render(view)
+
+    assert html
+           |> LazyHTML.from_document()
+           |> LazyHTML.query("input[name='author[name]']")
+           |> LazyHTML.attribute("value") == [author.name]
+
+    assert html
+           |> LazyHTML.from_document()
+           |> LazyHTML.query("input[name='author[email]']")
+           |> LazyHTML.attribute("value") == [author.email]
+
+    assert html
+           |> LazyHTML.from_document()
+           |> LazyHTML.query("input[name='author[bio]']")
+           |> LazyHTML.attribute("value") == [author.bio]
+
+    # Update with unique values
+    updated_name = "Updated-#{System.system_time(:nanosecond)}"
+    updated_bio = "Updated bio-#{System.system_time(:nanosecond)}"
+
+    # Submit the update
+    view
+    |> form("#auix-author-form",
+      author: %{
+        name: updated_name,
+        bio: updated_bio
+      }
+    )
+    |> render_submit()
+
+    # Verify the changes appear in the index
+    {:ok, _view, updated_html} = live(conn, "/ash-default-layout-authors")
+
+    assert updated_html =~ updated_name
+    assert updated_html =~ updated_bio
+  end
 end
