6f1909f57ef51100bfb715890dd389e5cdfb9b1c
Federico Alcantara <federico.alcantara@wadvanced.com>
2026-07-28 15:39:08 +0200

    feat(guides): showcase post topics in the Ash overview demo
    Wires the many-to-many `topics` association into the demo's post show and
    edit layouts, alongside `tags`, so the guide exercises the new membership UI.

--- lib/aurora_uix_web/guides/ash_overview.ex
+++ lib/aurora_uix_web/guides/ash_overview.ex
@@ -26,6 +26,7 @@ defmodule Aurora.UixWeb.Guides.AshOverview do
   alias Aurora.Uix.Guides.Blog.Comment
   alias Aurora.Uix.Guides.Blog.Post
   alias Aurora.Uix.Guides.Blog.Tag
+  alias Aurora.Uix.Guides.Blog.Topic
 
   auix_resource_metadata(:author__author_profile, ash_resource: AuthorProfile)
 
@@ -39,7 +40,11 @@ defmodule Aurora.UixWeb.Guides.AshOverview do
     field :description, html_type: :textarea
   end
 
-  auix_resource_metadata(:post, ash_resource: Post, order_by: :title)
+  auix_resource_metadata(:topics, ash_resource: Topic)
+
+  auix_resource_metadata(:post, ash_resource: Post, order_by: :title) do
+    field :topics, label: "Topics"
+  end
 
   auix_resource_metadata(:tag, ash_resource: Tag)
 
@@ -64,6 +69,11 @@ defmodule Aurora.UixWeb.Guides.AshOverview do
 
     show_layout :post do
       stacked([:status, :title, :author, :comment, :summary])
+
+      group "markers" do
+        inline([:tags])
+        inline([:topics])
+      end
     end
 
     edit_layout :post do
@@ -76,8 +86,12 @@ defmodule Aurora.UixWeb.Guides.AshOverview do
           stacked([:status, :published_at])
         end
 
+        group "markers" do
+          inline([:tags])
+          inline([:topics])
+        end
+
         inline([:summary])
-        inline([:tags])
       end
     end
   end
